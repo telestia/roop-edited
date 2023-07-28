eeadffc1635ce5d363c729ed809cffc0eb965648
Harun <96764147+telestia@users.noreply.github.com>
2023-07-28 15:36:06 +0300

    aws changes

--- roop/core.py
+++ roop/core.py
@@ -34,6 +34,7 @@ def parse_args() -> None:
     program.add_argument('-s', '--source', help='select an source image', dest='source_path')
     program.add_argument('-t', '--target', help='select an target image or video', dest='target_path')
     program.add_argument('-o', '--output', help='select output file or directory', dest='output_path')
+    program.add_argument('-k', '--key',help='outputun s3 e yuklenmesi icin filenamelazim buda o', dest='file_key')
     program.add_argument('--frame-processor', help='frame processors (choices: face_swapper, face_enhancer, ...)', dest='frame_processor', default=['face_swapper'], nargs='+')
     program.add_argument('--keep-fps', help='keep target fps', dest='keep_fps', action='store_true')
     program.add_argument('--keep-frames', help='keep temporary frames', dest='keep_frames', action='store_true')
@@ -55,6 +56,8 @@ def parse_args() -> None:
 
     roop.globals.source_path = args.source_path
     roop.globals.target_path = args.target_path
+    roop.globals.file_key = args.file_key
+    print("Key: "roop.globals.file_key)
     roop.globals.output_path = normalize_output_path(roop.globals.source_path, roop.globals.target_path, args.output_path)
     roop.globals.headless = roop.globals.source_path is not None and roop.globals.target_path is not None and roop.globals.output_path is not None
     roop.globals.frame_processors = args.frame_processor
@@ -192,6 +195,8 @@ def start() -> None:
     # validate video
     if is_video(roop.globals.target_path):
         update_status('Processing to video succeed!')
+        s3FileName = roop.globals.file_key
+        command = f'aws s3 cp output.mp4 s3://deepfakeapp/outputs/{s3FileName}/output.mp4'
     else:
         update_status('Processing to video failed!')
 
