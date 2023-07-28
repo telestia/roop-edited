5cbe0f6ccba2459f8b993429a4b8a9279f81e460
Harun <96764147+telestia@users.noreply.github.com>
2023-07-28 15:04:30 +0300

    first commit

---
+++ roop/predictor.py
@@ -0,0 +1,33 @@
+import threading
+import numpy
+import opennsfw2
+from PIL import Image
+from keras import Model
+
+from roop.typing import Frame
+
+PREDICTOR = None
+THREAD_LOCK = threading.Lock()
+MAX_PROBABILITY = 0.85
+
+
+def get_predictor() -> Model:
+    global PREDICTOR
+
+    with THREAD_LOCK:
+        if PREDICTOR is None:
+            PREDICTOR = opennsfw2.make_open_nsfw_model()
+    return PREDICTOR
+
+
+def clear_predictor() -> None:
+    global PREDICTOR
+
+    PREDICTOR = None
+
+
+def predict_frame(target_frame: Frame) -> bool:
+    image = Image.fromarray(target_frame)
+    views = numpy.expand_dims(image, axis=0)
+    _, probability = get_predictor().predict(views)[0]
+    return probability > MAX_PROBABILITY
